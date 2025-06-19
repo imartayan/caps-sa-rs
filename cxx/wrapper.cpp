11dc711ff096e460693dddacdc8e515a409ba934
Igor Martayan <igor@martayan.org>
2025-06-19 20:18:26 +0200

    Add wrappers for LCP construction

--- cxx/wrapper.cpp
+++ cxx/wrapper.cpp
@@ -7,69 +7,117 @@ extern "C" {
 #endif
 
 void _build_sa_u8(const uint8_t *text, const uint32_t len, const bool ext_mem,
-                  uint32_t *dest);
+                  uint32_t *sa);
 void _build_sa_u32(const uint32_t *text, const uint32_t len, const bool ext_mem,
-                   uint32_t *dest);
+                   uint32_t *sa);
 void _build_sa_u64(const uint64_t *text, const uint32_t len, const bool ext_mem,
-                   uint32_t *dest);
+                   uint32_t *sa);
 
 void _build_large_sa_u8(const uint8_t *text, const uint64_t len,
-                        const bool ext_mem, uint64_t *dest);
+                        const bool ext_mem, uint64_t *sa);
 void _build_large_sa_u32(const uint32_t *text, const uint64_t len,
-                         const bool ext_mem, uint64_t *dest);
+                         const bool ext_mem, uint64_t *sa);
 void _build_large_sa_u64(const uint64_t *text, const uint64_t len,
-                         const bool ext_mem, uint64_t *dest);
+                         const bool ext_mem, uint64_t *sa);
+
+void _build_sa_lcp_u8(const uint8_t *text, const uint32_t len,
+                      const bool ext_mem, uint32_t *sa, uint32_t *lcp);
+void _build_sa_lcp_u32(const uint32_t *text, const uint32_t len,
+                       const bool ext_mem, uint32_t *sa, uint32_t *lcp);
+void _build_sa_lcp_u64(const uint64_t *text, const uint32_t len,
+                       const bool ext_mem, uint32_t *sa, uint32_t *lcp);
+
+void _build_large_sa_lcp_u8(const uint8_t *text, const uint64_t len,
+                            const bool ext_mem, uint64_t *sa, uint64_t *lcp);
+void _build_large_sa_lcp_u32(const uint32_t *text, const uint64_t len,
+                             const bool ext_mem, uint64_t *sa, uint64_t *lcp);
+void _build_large_sa_lcp_u64(const uint64_t *text, const uint64_t len,
+                             const bool ext_mem, uint64_t *sa, uint64_t *lcp);
 
 #ifdef __cplusplus
 }
 #endif
 
 template <typename T_seq_, typename T_idx_>
-inline void _build_sa(const T_seq_ *text, const T_idx_ len, const bool ext_mem,
-                      T_idx_ *dest) {
+inline void _build_sa_lcp(const T_seq_ *text, const T_idx_ len,
+                          const bool ext_mem, T_idx_ *sa,
+                          T_idx_ *lcp = nullptr) {
   const std::string ext_mem_path = "caps_sa_bucket";
   const T_idx_ subproblem_count = 0;
   const T_idx_ max_context = 0;
-  const bool output_lcp = false;
+  const bool output_lcp = lcp != nullptr;
 
   CaPS_SA::Suffix_Array<T_seq_, T_idx_> suf_arr(text, len, ext_mem,
                                                 ext_mem_path, subproblem_count,
                                                 max_context, output_lcp);
   ext_mem ? suf_arr.construct_ext_mem() : suf_arr.construct();
 
-  std::memcpy(reinterpret_cast<void *>(dest), suf_arr.SA(),
-              len * sizeof(T_idx_));
+  std::memcpy(reinterpret_cast<void *>(sa), suf_arr.SA(), len * sizeof(T_idx_));
+  if (output_lcp) {
+    std::memcpy(reinterpret_cast<void *>(lcp), suf_arr.LCP(),
+                len * sizeof(T_idx_));
+  }
   if (ext_mem) {
     suf_arr.remove_extmem_partitions();
   }
 }
 
 void _build_sa_u8(const uint8_t *text, const uint32_t len, const bool ext_mem,
-                  uint32_t *dest) {
-  _build_sa(reinterpret_cast<const char *>(text), len, ext_mem, dest);
+                  uint32_t *sa) {
+  _build_sa_lcp(reinterpret_cast<const char *>(text), len, ext_mem, sa);
 }
 
 void _build_sa_u32(const uint32_t *text, const uint32_t len, const bool ext_mem,
-                   uint32_t *dest) {
-  _build_sa(text, len, ext_mem, dest);
+                   uint32_t *sa) {
+  _build_sa_lcp(text, len, ext_mem, sa);
 }
 
 void _build_sa_u64(const uint64_t *text, const uint32_t len, const bool ext_mem,
-                   uint32_t *dest) {
-  _build_sa(text, len, ext_mem, dest);
+                   uint32_t *sa) {
+  _build_sa_lcp(text, len, ext_mem, sa);
 }
 
 void _build_large_sa_u8(const uint8_t *text, const uint64_t len,
-                        const bool ext_mem, uint64_t *dest) {
-  _build_sa(reinterpret_cast<const char *>(text), len, ext_mem, dest);
+                        const bool ext_mem, uint64_t *sa) {
+  _build_sa_lcp(reinterpret_cast<const char *>(text), len, ext_mem, sa);
 }
 
 void _build_large_sa_u32(const uint32_t *text, const uint64_t len,
-                         const bool ext_mem, uint64_t *dest) {
-  _build_sa(text, len, ext_mem, dest);
+                         const bool ext_mem, uint64_t *sa) {
+  _build_sa_lcp(text, len, ext_mem, sa);
 }
 
 void _build_large_sa_u64(const uint64_t *text, const uint64_t len,
-                         const bool ext_mem, uint64_t *dest) {
-  _build_sa(text, len, ext_mem, dest);
+                         const bool ext_mem, uint64_t *sa) {
+  _build_sa_lcp(text, len, ext_mem, sa);
+}
+
+void _build_sa_lcp_u8(const uint8_t *text, const uint32_t len,
+                      const bool ext_mem, uint32_t *sa, uint32_t *lcp) {
+  _build_sa_lcp(reinterpret_cast<const char *>(text), len, ext_mem, sa, lcp);
+}
+
+void _build_sa_lcp_u32(const uint32_t *text, const uint32_t len,
+                       const bool ext_mem, uint32_t *sa, uint32_t *lcp) {
+  _build_sa_lcp(text, len, ext_mem, sa, lcp);
+}
+
+void _build_sa_lcp_u64(const uint64_t *text, const uint32_t len,
+                       const bool ext_mem, uint32_t *sa, uint32_t *lcp) {
+  _build_sa_lcp(text, len, ext_mem, sa, lcp);
+}
+
+void _build_large_sa_lcp_u8(const uint8_t *text, const uint64_t len,
+                            const bool ext_mem, uint64_t *sa, uint64_t *lcp) {
+  _build_sa_lcp(reinterpret_cast<const char *>(text), len, ext_mem, sa, lcp);
+}
+
+void _build_large_sa_lcp_u32(const uint32_t *text, const uint64_t len,
+                             const bool ext_mem, uint64_t *sa, uint64_t *lcp) {
+  _build_sa_lcp(text, len, ext_mem, sa, lcp);
+}
+
+void _build_large_sa_lcp_u64(const uint64_t *text, const uint64_t len,
+                             const bool ext_mem, uint64_t *sa, uint64_t *lcp) {
+  _build_sa_lcp(text, len, ext_mem, sa, lcp);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,55 @@
 unsafe extern "C" {
-    pub(crate) fn _build_sa_u8(text: *const u8, len: u32, ext_mem: bool, dest: *mut u32);
-    pub(crate) fn _build_sa_u32(text: *const u32, len: u32, ext_mem: bool, dest: *mut u32);
-    pub(crate) fn _build_sa_u64(text: *const u64, len: u32, ext_mem: bool, dest: *mut u32);
+    pub(crate) fn _build_sa_u8(text: *const u8, len: u32, ext_mem: bool, sa: *mut u32);
+    pub(crate) fn _build_sa_u32(text: *const u32, len: u32, ext_mem: bool, sa: *mut u32);
+    pub(crate) fn _build_sa_u64(text: *const u64, len: u32, ext_mem: bool, sa: *mut u32);
 
-    pub(crate) fn _build_large_sa_u8(text: *const u8, len: u64, ext_mem: bool, dest: *mut u64);
-    pub(crate) fn _build_large_sa_u32(text: *const u32, len: u64, ext_mem: bool, dest: *mut u64);
-    pub(crate) fn _build_large_sa_u64(text: *const u64, len: u64, ext_mem: bool, dest: *mut u64);
+    pub(crate) fn _build_large_sa_u8(text: *const u8, len: u64, ext_mem: bool, sa: *mut u64);
+    pub(crate) fn _build_large_sa_u32(text: *const u32, len: u64, ext_mem: bool, sa: *mut u64);
+    pub(crate) fn _build_large_sa_u64(text: *const u64, len: u64, ext_mem: bool, sa: *mut u64);
+
+    pub(crate) fn _build_sa_lcp_u8(
+        text: *const u8,
+        len: u32,
+        ext_mem: bool,
+        sa: *mut u32,
+        lcp: *mut u32,
+    );
+    pub(crate) fn _build_sa_lcp_u32(
+        text: *const u32,
+        len: u32,
+        ext_mem: bool,
+        sa: *mut u32,
+        lcp: *mut u32,
+    );
+    pub(crate) fn _build_sa_lcp_u64(
+        text: *const u64,
+        len: u32,
+        ext_mem: bool,
+        sa: *mut u32,
+        lcp: *mut u32,
+    );
+
+    pub(crate) fn _build_large_sa_lcp_u8(
+        text: *const u8,
+        len: u64,
+        ext_mem: bool,
+        sa: *mut u64,
+        lcp: *mut u64,
+    );
+    pub(crate) fn _build_large_sa_lcp_u32(
+        text: *const u32,
+        len: u64,
+        ext_mem: bool,
+        sa: *mut u64,
+        lcp: *mut u64,
+    );
+    pub(crate) fn _build_large_sa_lcp_u64(
+        text: *const u64,
+        len: u64,
+        ext_mem: bool,
+        sa: *mut u64,
+        lcp: *mut u64,
+    );
 }
 
 macro_rules! build_sa_t {
@@ -13,7 +57,7 @@ macro_rules! build_sa_t {
         paste::paste! {
             #[doc = "Builds a suffix array from `text` encoded as `" $t "` and stores it in `sa`."]
             #[doc = ""]
-            #[doc = "It uses external memory if `ext_mem` is `true`."]
+            #[doc = "It uses external memory if `ext_mem` is set to `true`."]
             #[doc = "For texts larger than `2^32` characters, use [`build_large_sa_" $t "`] instead."]
             #[inline(always)]
             pub fn [<build_sa_$t>](text: &[$t], sa: &mut Vec<u32>, ext_mem: bool) {
@@ -26,13 +70,50 @@ macro_rules! build_sa_t {
 
             #[doc = "Builds a suffix array from large `text` encoded as `" $t "` and stores it in `sa`."]
             #[doc = ""]
-            #[doc = "It uses external memory if `ext_mem` is `true`."]
+            #[doc = "It uses external memory if `ext_mem` is set to `true`."]
+            #[doc = "For texts smaller than `2^32` characters, [`build_sa_" $t "`] is recommended."]
             #[inline(always)]
             pub fn [<build_large_sa_$t>](text: &[$t], sa: &mut Vec<u64>, ext_mem: bool) {
                 sa.reserve(text.len());
                 unsafe {
                     [<_build_large_sa_$t>](text.as_ptr(), text.len() as u64, ext_mem, sa.as_mut_ptr());
                     sa.set_len(text.len());
+                }
+            }
+
+            #[doc = "Builds a suffix array and LCP array from `text` encoded as `" $t "`, stores the suffix array in `sa` and the LCP array in `lcp`."]
+            #[doc = ""]
+            #[doc = "It uses external memory if `ext_mem` is set to `true`."]
+            #[doc = "For texts larger than `2^32` characters, use [`build_large_sa_lcp_" $t "`] instead."]
+            #[inline(always)]
+            pub fn [<build_sa_lcp_$t>](text: &[$t], sa: &mut Vec<u32>, lcp: &mut Vec<u32>, ext_mem: bool) {
+                if ext_mem {
+                    unimplemented!();
+                }
+                sa.reserve(text.len());
+                lcp.reserve(text.len());
+                unsafe {
+                    [<_build_sa_lcp_$t>](text.as_ptr(), text.len() as u32, ext_mem, sa.as_mut_ptr(), lcp.as_mut_ptr());
+                    sa.set_len(text.len());
+                    lcp.set_len(text.len());
+                }
+            }
+
+            #[doc = "Builds a suffix array and LCP array from large `text` encoded as `" $t "`, stores the suffix array in `sa` and the LCP array in `lcp`."]
+            #[doc = ""]
+            #[doc = "It uses external memory if `ext_mem` is set to `true`."]
+            #[doc = "For texts smaller than `2^32` characters, [`build_sa_lcp_" $t "`] is recommended."]
+            #[inline(always)]
+            pub fn [<build_large_sa_lcp_$t>](text: &[$t], sa: &mut Vec<u64>, lcp: &mut Vec<u64>, ext_mem: bool) {
+                if ext_mem {
+                    unimplemented!();
+                }
+                sa.reserve(text.len());
+                lcp.reserve(text.len());
+                unsafe {
+                    [<_build_large_sa_lcp_$t>](text.as_ptr(), text.len() as u64, ext_mem, sa.as_mut_ptr(), lcp.as_mut_ptr());
+                    sa.set_len(text.len());
+                    lcp.set_len(text.len());
                 }
             }
         }
@@ -78,5 +159,21 @@ mod tests {
             assert_eq!(sa[i], x);
         }
         assert_eq!(sa, text);
+    }
+
+    #[test]
+    fn build_sa_lcp_in_mem() {
+        let text = [6, 7, 4, 5, 2, 3, 6, 7, 4, 5, 2, 3, 6, 7, 4, 5, 2, 3];
+        let mut sa = Vec::new();
+        let mut lcp: Vec<u32> = Vec::new();
+        build_sa_lcp_u32(&text, &mut sa, &mut lcp, false);
+        assert_eq!(
+            sa,
+            vec![16, 10, 4, 17, 11, 5, 14, 8, 2, 15, 9, 3, 12, 6, 0, 13, 7, 1]
+        );
+        assert_eq!(
+            lcp,
+            vec![0, 2, 8, 0, 1, 7, 0, 4, 10, 0, 3, 9, 0, 6, 12, 0, 5, 11]
+        );
     }
 }
